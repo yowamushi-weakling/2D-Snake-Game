999ee8e1c55c51b2a8d0d159ceee1d8639e373af
yowamushi-weakling <73294938+yowamushi-weakling@users.noreply.github.com>
2020-11-18 12:40:19 +0700

    Final Project
    Including:
    - Snake
    - Food
    - Border

---
+++ 2D Snake Game/Assets/Snake.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Snake : MonoBehaviour
+{
+    Vector2 dir = Vector2.right;
+
+    List<Transform> tail = new List<Transform>();
+
+    bool ate = false;
+
+    public GameObject tailPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Move", 0.3f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = -Vector2.up;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = -Vector2.right;
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector2.up;
+    }
+
+    void Move()
+    {
+        // Save current position
+        Vector2 v = transform.position;
+
+        // Move head into new direction
+        transform.Translate(dir);
+
+        if (ate)
+        {
+            GameObject g = (GameObject)Instantiate(tailPrefab, v, Quaternion.identity);
+        }
+
+        if (tail.Count > 0)
+        {
+            // Move last tail element to where the head was
+            tail.Last().position = v;
+
+            // Add to front of list, remove from the back
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.StartsWith("FoodPrefab"))
+        {
+            ate = true;
+
+            Destroy(collision.gameObject);
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ 2D Snake Game/Assets/SpawnFood.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    public GameObject foodPrefeb;
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    // Spawn one piece of food
+    void Spawn()
+    {
+        // x position between left & right border
+        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.y);
+
+        // y position between top & bottom border
+        int y = (int)Random.Range(borderBottom.position.y, borderTop.position.y);
+
+        // Instantiate the food at (x, y)
+        Instantiate(foodPrefeb, new Vector2(x, y), Quaternion.identity); // default rotation
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn food every 4 seconds, starting in 3
+        InvokeRepeating("Spawn", 3, 4);
+    }
+}
